d72583eeb8214fafc82d0ec6523e58eff75a5953
yrsong15 <ys101@duke.edu>
2016-07-27 21:06:09 +0000

    Modeling user - incomplete

---
+++ test/models/user_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  
+  def setup
+    @user = User.new(name:"Ex Ample", email:"example@duke.edu")
+  end
+  
+  test "should be valid" do
+    assert @user.valid?
+  end
+  
+  test "name should be present" do
+    @user.name = "          "
+    assert_not @user.valid?
+  end
+end
