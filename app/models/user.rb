a150b6fe9dc19240f242e5876dda89a6f3fef8fc
yrsong15 <ys101@duke.edu>
2016-07-28 20:54:04 +0000

    updated password regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   #this is customized for Duke netIDs: 2 characters followed by 3 numbers
-  VALID_DUKE_EMAIL_REGEX = /\A^[a-z]{2}+[0-9]{3}+@duke+\.edu+\z/i
+  VALID_DUKE_EMAIL_REGEX = /\A^[a-z]{2,}+[0-9]{1,}+@duke+\.edu+\z/i
   
   validates :email, presence: true, length:{maximum:255},
     format:{with: VALID_DUKE_EMAIL_REGEX},
